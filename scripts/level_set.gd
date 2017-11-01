0b8e67e927b3acc7988da4b8d5d5aabaf3958d96
Dave Jeffery <david.richard.jeffery@gmail.com>
2017-11-01 11:05:01 +0100

    Added level_set.gd

---
+++ scripts/level_set.gd
@@ -0,0 +1,33 @@
+#script: level_set.gd
+
+extends Node
+
+# Default values for game data
+const POSITION = Vector2(0, 0)		# Default position for Dave
+const LEVEL_SIZE = Vector2(16, 12)	# Default level size
+const MIN_SCORE = 0					# Default minimum score
+const MAP_CONTENT = [0, 0]			# Default map square content [Object, Target (optional)]
+
+var minimum_score 
+var level_size      
+var dave_pos
+
+func _ready():
+	# Called every time the node is added to the scene.
+	
+	# Arrays that store data for each map in current set
+	map = [create_map(LEVEL_SIZE)]
+	dave_pos = [POSITION]
+	level_size = [LEVEL_SIZE]
+	minimum_score = [MIN_SCORE]
+
+func create_map(size):
+	# Creates a blank map of size
+	var map = []
+	
+	for x in range(size.x):
+		var col = []
+		col.resize(size.y)
+		map.append(col)
+
+		return map
