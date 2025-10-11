965df019a152ee8b68d20eb4992f90ec74415ccc
Dave Jeffery <dave@kecskebak.hu>
2025-10-11 07:29:56 +0200

    Statically typed direction.gd

--- scripts/direction.gd
+++ scripts/direction.gd
@@ -2,17 +2,17 @@
 
 extends Node
 
-const NORTH = Vector2(0, 1)
-const SOUTH = Vector2(0, -1)
-const EAST = Vector2(1, 0)
-const WEST = Vector2(-1, 0)
+# direction.gd
+const NORTH := Vector2i.UP
+const SOUTH := Vector2i.DOWN
+const EAST  := Vector2i.RIGHT
+const WEST  := Vector2i.LEFT
+const NE := NORTH + EAST
+const NW := NORTH + WEST
+const SE := SOUTH + EAST
+const SW := SOUTH + WEST
 
-const NE = NORTH + EAST
-const NW = NORTH + WEST
-const SW = SOUTH + WEST
-const SE = EAST + WEST
-
-func _ready():
+func _ready() -> void:
 	# Called every time the node is added to the scene.
 	# Initialization here
 	pass
