13343ede682f382b8b958c4c8ad74b4632297427
Dave Jeffery <dave@kecskebak.hu>
2025-10-19 19:18:09 +0200

    Added LevelData class

---
+++ scripts/level_data.gd
@@ -0,0 +1,11 @@
+extends Resource
+class_name LevelData
+# Optional: give a custom resource type id so it shows nicely in the "New Resource" dialog.
+# @export_category("Level")
+
+# Use typed properties so loaded resource gives you properly typed fields.
+@export var level_map: Array = []                # keep nested arrays (tile pairs etc.)
+@export var def_file: String = ""
+@export var level_size: Array[Vector2i] = []     # Array of Vector2i
+@export var dave_pos: Array[Vector2i] = []       # Array of Vector2i
+@export var minimum_score: Array[int] = []       # Array of int
