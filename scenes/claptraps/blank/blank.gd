c25f4addf952ba1bf8cf12d2dee59e904d56a0ef
Dave Jeffery <dave@kecskebak.hu>
2025-10-21 18:48:10 +0200

    Added Wall and Blank

---
+++ scenes/claptraps/blank/blank.gd
@@ -0,0 +1,9 @@
+extends Thing
+
+func _ready() -> void:
+	game_object_name = "Blank"
+	sprite = 0 #NOTE Probably not needed
+	solid = false
+	squash = true
+	empty = true
+	solid_to_red_frog = false
---
+++ scenes/claptraps/wall/wall.gd
@@ -0,0 +1,5 @@
+extends Thing
+
+func _ready() -> void:
+	break_box = true
+	game_object_name = "Wall"
