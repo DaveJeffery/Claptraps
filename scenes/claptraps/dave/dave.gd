833e0aa0c277ab7c525708ea3c1f6b681c0938e1
Dave Jeffery <dave@kecskebak.hu>
2025-10-21 16:02:34 +0200

    Added Dave animation

--- scenes/claptraps/dave/dave.gd
+++ scenes/claptraps/dave/dave.gd
@@ -4,7 +4,7 @@ extends Thing
 func _ready() -> void:
 	game_object = 0
 	is_dave = true
-	game_object_name = 'Dave'
+	game_object_name = "Dave"
 	startle_frog = true
 	trigger_button = true
 	solid_to_red_frog = false
@@ -13,18 +13,12 @@ func _ready() -> void:
 # so you can put goal checks and things in here
 func action() -> void:
 	if game_state.min_score_hit:
-		game_state.message = 'Get to the Chopper!'
-		col_a, col_b, col_c = game_state.bg_col
-		if game_state.bg_col[0] < 255:
-			col_a += 2
-			if col_a > 255:
-				col_a = 255
-		if game_state.bg_col[1] < 220:
-			col_b += 2
-			if col_b > 220:
-				col_b = 220
-		if game_state.bg_col[2] > 150:
-			col_c -= 2
-			if col_c < 150:
-				col_c = 150
-		game_state.bg_col = col_a, col_b, col_c
+		game_state.message = "EPISODE_COMPLETE"
+			
+		# Fade background to orange
+		if game_state.bg_col.r8 < 255:
+			game_state.bg_col.r8 += 2
+		if game_state.bg_col.g8 < 220:
+			game_state.bg_col.g8 += 2
+		if game_state.bg_col.b8 > 150:
+			game_state.bg_col.b8 -= 2
