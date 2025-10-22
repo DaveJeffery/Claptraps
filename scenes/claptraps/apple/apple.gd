30736ded1b84b08c89f177617a42e0e756333ff1
Dave Jeffery <dave@kecskebak.hu>
2025-10-22 19:15:45 +0200

    Added gate and apple

---
+++ scenes/claptraps/apple/apple.gd
@@ -0,0 +1,68 @@
+class_name Apple
+extends Thing
+
+const APPLE_FALLS := [
+	"Frog",
+	"Startled_Frog",
+	"Red_Frog",
+	"Angry_Red_Frog",
+	"Laser",
+]
+
+var life: int
+
+func _ready() -> void:
+	game_object= 14
+	game_object_name = "Apple"
+	h_push = true
+	move_speed = 4
+	life = 60
+
+func action() -> void:
+
+	if moving > 0 or moved:
+		startle_frog = true
+	else:
+		startle_frog = false
+
+	if (
+		look(game_state.SOUTH, grid_pos).empty 
+		or look(game_state.SOUTH, grid_pos).name in APPLE_FALLS
+	):
+		move(game_state.SOUTH, self, grid_pos)
+	elif (
+		look(game_state.SOUTH, grid_pos).name == "Apple" 
+		and (
+			look(game_state.SW, grid_pos).empty 
+			or look(game_state.SW, grid_pos).name in APPLE_FALLS
+			) 
+		and (
+			look(game_state.WEST, grid_pos).empty 
+			or look(game_state.WEST, grid_pos).name in APPLE_FALLS
+			)
+	):
+		move(game_state.WEST, self, grid_pos)
+
+	elif (
+		look(game_state.SOUTH, grid_pos).name == "Apple" 
+		and (
+			look(game_state.SE, grid_pos).empty 
+			or look(game_state.SE, grid_pos).name in APPLE_FALL
+		) 
+		and (
+			look(game_state.EAST, grid_pos).empty 
+			or look(game_state.EAST, grid_pos).name in APPLE_FALLS
+		)
+	):
+		move(game_state.EAST, self, grid_pos)
+
+	if (
+		moved == game_state.SOUTH 
+		and look(game_state.SOUTH, grid_pos).is_dave
+	):
+		game_state.kill_dave = true
+
+func eat() -> void:
+	life -= 1
+	if life < 0:
+		create("Blank", 0, grid_pos)
---
+++ scenes/claptraps/gate/gate.gd
@@ -0,0 +1,7 @@
+class_name  Gate
+extends Thing
+
+func _ready() -> void:
+	game_object = 13
+	game_object_name= "Gate"
+	break_box = true
